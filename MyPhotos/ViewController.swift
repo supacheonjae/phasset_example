9c0303f0bb6205e66f3515f803515c497477974b
supa cheon-jae <34002827+supacheonjae@users.noreply.github.com>
2024-09-07 11:44:05 +0900

    Initial Commit

---
+++ MyPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPhotos
+//
+//  Created by Yun Ha on 2024/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
